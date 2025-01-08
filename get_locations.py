d7360b366e45dd3ff0a40e07719def92900a3812
Ofri <ofri.naftali.46@gmail.com>
2025-01-08 09:34:18 +0200

    This is an updated commit with implemented objects, Rocket and Points

--- get_locations.py
+++ get_locations.py
@@ -1,19 +1,14 @@
 import pandas as pd
 from typing import Tuple,List
 import math
+import numpy as np
+
+EARTH_RADIUS = 6378137
 class Radar:
-    _elevation = 0
+    elevation = 0
     def __init__(self, longitude, latitude):
-        self._longitude = longitude
-        self._latitude = latitude
-    def longitude(self):
-        return self._longitude
-    def latitude(self):
-        return self._latitude
-    def elevation(self):
-        return self._elevation
-    def get_location(self):
-        return self._longitude, self._latitude, self._elevation
+        self.longitude = longitude
+        self.latitude = latitude
 
 class Point:
     def __init__(self, longitude, latitude, altitude):
@@ -26,7 +21,27 @@ class RadarPoint:
         self.range = r
         self.radar = radar
 
+
+def lat_calculate(r: RadarPoint) -> float:
+    lat_diff = math.atan((r.range*math.cos(np.radians(r.elevation))*math.cos(np.radians(r.azimuth)))/EARTH_RADIUS)
+    return lat_diff + r.radar.latitude
+
+def lon_calculate(r: RadarPoint) -> float:
+    lon_diff = math.atan((r.range*math.cos(np.radians(r.elevation))*math.sin(np.radians(r.azimuth)))/EARTH_RADIUS)
+    return lon_diff + r.radar.latitude
+
+
+def altitude_calculate(r: RadarPoint) -> float:
+    dis_from_center = math.sqrt(EARTH_RADIUS**2 + r.elevation**2 + 2*EARTH_RADIUS*r.range*math.sin(np.radians(r.elevation)))
+    return dis_from_center - EARTH_RADIUS
+
+
+def process_point(radar_point: RadarPoint) -> Point:
+    return Point(longitude=lon_calculate(radar_point), latitude=lat_calculate(radar_point), altitude=altitude_calculate(radar_point))
+
+
 class Rocket:
+
     _unprocessed_locations : List[RadarPoint] = []
     _processed_locations : List[Point] = []
 
@@ -38,15 +53,7 @@ class Rocket:
 
     def add_point(self, radar_point: RadarPoint) -> None:
         self._unprocessed_locations.append(radar_point)
-        self._processed_locations.append(self.process_point(radar_point))
+        self._processed_locations.append(process_point(radar_point))
 
 
-    def process_point(self, radar_point: RadarPoint) -> Point:
-        pass
 
-    def lat_calculate(self, radar_point: RadarPoint) -> float:
-        lat_diff = math.atan((radar_point.range))
-
-    def lon_calculate(self, radar_point: RadarPoint) -> float:
-
-    def altitude_calculate(self, radar_point: RadarPoint) -> float:
