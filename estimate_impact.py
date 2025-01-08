dfa8bdd007a97ca7ebb056cad89979cbc7545f33
matarjarden <matar.jarden.46@gmail.com>
2025-01-08 15:26:36 +0200

    buijnkml

--- estimate_impact.py
+++ estimate_impact.py
@@ -4,19 +4,27 @@ import matplotlib.pyplot as plt
 import get_points_from_data
 
 
-def createG(time, point3, v1, v2):
-    displacement = [point3.get_altitude()]
+def createG(time, point3, point2, point1):
+    v1_alt =
+    v2_alt =
+    v1_long =
+    v2_long =
+    v1_lat =
+    v2_lat =
+    alt = [point3.get_altitude()]
+    long = [point3.get_longitude()]
+    lat = [point3.get_latitude()]
     for n in range(0, len(time) - 1):
-        displacement.append(displacement[-1] + 0.5 * (v2 + v1) * (time[n + 1] - time[n])- 9.81* (time[n + 1] - time[n])*2)
+        alt.append(alt[-1] + 0.5 * (v2 + v1) * (time[n + 1] - time[n])- 9.81* (time[n + 1] - time[n])*2)
         v1 = v2
-        v2 = (displacement[n] - displacement[n-1]) / (time[n+1] - time[n])
-        if displacement[n+1] < 0:
-            return np.array(displacement)
-    return np.array(displacement)
+        v2 = (alt[n] - alt[n-1]) / (time[n+1] - time[n])
+        if alt[n+1] < 0:
+            return np.array(alt)
+    return np.array(alt)
 
 
 points = get_points_from_data.get_data()[1].get_locations()
-points = points[:6]
+points = points[:200]
 disp = []
 for i in range(len(points) - 2):
     point1 = points[i]
@@ -35,8 +43,16 @@ for i in range(len(points) - 2):
         disp_corrected[j] = point_G_prediction[j]
     disp.append(disp_corrected)
 
+avg = 0
+ground = 0
+for i in range(len(disp)):
+    for j in range(len(disp[i])):
+        if disp[i][j] < 0:
+            avg = avg + (time[i] + time[i-1])/2
+avg = avg/(len(disp))
+print(avg)
 
-plt.plot(time, disp[1])
+plt.plot(time - 1736300020, disp[0])
 plt.legend(["Displacement", "Velocity"])
 plt.xlabel("Time")
 plt.show()
